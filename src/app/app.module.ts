c2ece5e3abfc16da2640e1101c0109e75e48ee33
Nikki Johnson <nikkijohnson@50-207-252-218-static.hfc.comcastbusiness.net>
2019-04-30 15:40:37 -0600

    finished app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { ListManagerComponent } from './list-manager/list-manager.component';
+import { TodoListService } from './services/todo-list.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputButtonUnitComponent,
+    TodoItemComponent,
+    ListManagerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [TodoListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
